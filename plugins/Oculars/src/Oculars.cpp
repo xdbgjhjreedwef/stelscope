Stellarium/stellarium

commit 7d31bbb640549cad06309d59ac711ff62ca314f9

Oculars improvements
- Separate font scaling for the Oculars GUI panel.
- Storing of current instrument (telescope/ocular/CCD/lens) indices
- removed doubled parenting code

plugins/Oculars/src/Oculars.cpp
@@ -165,6 +165,7 @@ Oculars::Oculars()
 	, actionOcularIncrement(Q_NULLPTR)
 	, actionOcularDecrement(Q_NULLPTR)
 	, guiPanel(Q_NULLPTR)
+	, guiPanelFontSize(12)
 	, actualFOV(0.)
 	, initialFOV(0.)
 	, flagInitFOVUsage(false)
@@ -279,6 +280,10 @@ void Oculars::deinit()
 	settings->setValue("telescope_count", telescopes.count());
 	settings->setValue("ccd_count", ccds.count());
 	settings->setValue("lens_count", lenses.count());
+	settings->setValue("ocular_index", selectedOcularIndex);
+	settings->setValue("telescope_index", selectedTelescopeIndex);
+	settings->setValue("ccd_index", selectedCCDIndex);
+	settings->setValue("lens_index", selectedLensIndex);
 
 	StelCore *core = StelApp::getInstance().getCore();
 	StelSkyDrawer *skyDrawer = core->getSkyDrawer();
@@ -625,7 +630,7 @@ void Oculars::init()
 		}
 		else
 		{
-			selectedOcularIndex = 0;
+			selectedOcularIndex = qMin(settings->value("ocular_index", 0).toInt(), actualOcularCount-1);
 		}
 
 		int ccdCount = settings->value("ccd_count", 0).toInt();
@@ -647,6 +652,7 @@ void Oculars::init()
 			qWarning() << "The Oculars ini file appears to be corrupt; delete it.";
 			ready = false;
 		}
+		selectedCCDIndex = qMin(settings->value("ccd_index", 0).toInt(), actualCcdCount-1);
 
 		int telescopeCount = settings->value("telescope_count", 0).toInt();
 		int actualTelescopeCount = telescopeCount;
@@ -676,7 +682,7 @@ void Oculars::init()
 		}
 		else
 		{
-			selectedTelescopeIndex = 0;
+			selectedTelescopeIndex = qMin(settings->value("telescope_index", 0).toInt(), actualTelescopeCount-1);;
 		}
 
 		int lensCount = settings->value("lens_count", 0).toInt();
@@ -697,6 +703,7 @@ void Oculars::init()
 		{
 			qWarning() << "The Oculars ini file appears to be corrupt; delete it.";
 		}
+		selectedLensIndex=qMin(settings->value("lens_index", 0).toInt(), actualLensCount-1);;
 
 		pxmapGlow = new QPixmap(":/graphicGui/glow32x32.png");
 		pxmapOnIcon = new QPixmap(":/ocular/bt_ocular_on.png");
@@ -706,6 +713,7 @@ void Oculars::init()
 		initializeActivationActions();
 		determineMaxEyepieceAngle();
 
+		guiPanelFontSize=settings->value("gui_panel_fontsize", 12).toInt();
 		enableGuiPanel(settings->value("enable_control_panel", true).toBool());
 
 		// This must come ahead of setFlagAutosetMountForCCD (GH #505)
@@ -2647,4 +2655,23 @@ void Oculars::setFlagShowOcularsButton(bool b)
 	settings->sync();
 
 	emit flagShowOcularsButtonChanged(b);
+}
+
+
+void Oculars::setGuiPanelFontSize(int size)
+{
+	// This forces a redraw of the panel.
+	if (size!=guiPanelFontSize)
+	{
+		bool guiPanelVisible=guiPanel;
+		if (guiPanelVisible)
+			enableGuiPanel(false);
+		guiPanelFontSize=size;
+		if (guiPanelVisible)
+			enableGuiPanel(true);
+
+		settings->setValue("gui_panel_fontsize", size);
+		settings->sync();
+		emit guiPanelFontSizeChanged(size);
+	}
 }

plugins/Oculars/src/Oculars.hpp
@@ -103,6 +103,7 @@ class Oculars : public StelModule
 	Q_PROPERTY(bool flagShowOcularsButton	READ getFlagShowOcularsButton  WRITE setFlagShowOcularsButton  NOTIFY flagShowOcularsButtonChanged)
 
 	Q_PROPERTY(double arrowButtonScale     READ getArrowButtonScale        WRITE setArrowButtonScale        NOTIFY arrowButtonScaleChanged)
+	Q_PROPERTY(int guiPanelFontSize        READ getGuiPanelFontSize        WRITE setGuiPanelFontSize        NOTIFY guiPanelFontSizeChanged)
 
 	//BM: Temporary, until the GUI is finalized and some other method of getting
 	//info from the main class is implemented.
@@ -183,6 +184,8 @@ public slots:
 	
 	void enableGuiPanel(bool enable = true);
 	bool getFlagGuiPanelEnabled(void) const {return flagGuiPanelEnabled;}
+	void setGuiPanelFontSize(int size);
+	int getGuiPanelFontSize()const {return guiPanelFontSize;}
 
 	void setFlagDMSDegrees(const bool b);
 	bool getFlagDMSDegrees(void) const;
@@ -239,6 +242,7 @@ signals:
 	void selectedCCDRotationAngleChanged(double value);
 
 	void flagGuiPanelEnabledChanged(bool value);
+	void guiPanelFontSizeChanged(int value);
 	void flagHideGridsLinesChanged(bool value);
 	void flagAutosetMountForCCDChanged(bool value);
 	void flagScalingFOVForTelradChanged(bool value);
@@ -399,6 +403,7 @@ private:
 	StelAction * actionOcularDecrement;
 
 	class OcularsGuiPanel * guiPanel;
+	int guiPanelFontSize;
 
 	//Reticle
 	StelTextureSP reticleTexture;

plugins/Oculars/src/gui/OcularDialog.cpp
@@ -292,6 +292,7 @@ void OcularDialog::createDialogContent()
 	connect(ui->TitleBar, SIGNAL(movedTo(QPoint)), this, SLOT(handleMovedTo(QPoint)));
 
 	connectBoolProperty(ui->checkBoxControlPanel,          "Oculars.flagGuiPanelEnabled");
+	connectIntProperty(ui->guiFontSizeSpinBox,             "Oculars.guiPanelFontSize");
 	connectBoolProperty(ui->checkBoxInitialFOV,            "Oculars.flagInitFOVUsage");
 	connectBoolProperty(ui->checkBoxInitialDirection,      "Oculars.flagInitDirectionUsage");
 	connectBoolProperty(ui->checkBoxResolutionCriterion,   "Oculars.flagShowResolutionCriterions");

plugins/Oculars/src/gui/OcularsGuiPanel.cpp
@@ -44,9 +44,7 @@ OcularsGuiPanel::OcularsGuiPanel(Oculars* plugin,
 	parentWidget(parent),
 	borderPath(Q_NULLPTR)
 {
-	setMaximumSize(300, 450);
 	setContentsMargins(0, 0, 0, 0);
-	//TODO: set font?
 
 	//First create the layout and populate it, then set it?
 	mainLayout = new QGraphicsLinearLayout(Qt::Vertical);
@@ -64,7 +62,6 @@ OcularsGuiPanel::OcularsGuiPanel(Oculars* plugin,
 				      ocularsPlugin->actionShowOcular,
 				      true); //No background
 	buttonOcular->setToolTip(ocularsPlugin->actionShowOcular->getText());
-	buttonOcular->setParentItem(buttonBar);
 
 	Q_ASSERT(ocularsPlugin->actionShowCrosshairs);
 	buttonCrosshairs = new StelButton(buttonBar,
@@ -111,16 +108,12 @@ OcularsGuiPanel::OcularsGuiPanel(Oculars* plugin,
 
 	//Widgets with control and information fields
 	ocularControls = new QGraphicsWidget(this);
-	ocularControls->setParentItem(this);
 	ocularControls->setVisible(false);
 	lensControls = new QGraphicsWidget(this);
-	lensControls->setParentItem(this);
 	lensControls->setVisible(false);
 	ccdControls = new QGraphicsWidget(this);
-	ccdControls->setParentItem(this);
 	ccdControls->setVisible(false);
 	telescopeControls = new QGraphicsWidget(this);
-	telescopeControls->setParentItem(this);
 	telescopeControls->setVisible(false);
 
 	fieldOcularName = new QGraphicsTextItem(ocularControls);
@@ -145,10 +138,8 @@ OcularsGuiPanel::OcularsGuiPanel(Oculars* plugin,
 	fieldLensMultipler = new QGraphicsTextItem(lensControls);
 
 	QFont newFont = font();
-	// Font size is 12
-	newFont.setPixelSize(StelApp::getInstance().getBaseFontSize()-1);
+	newFont.setPixelSize(plugin->getGuiPanelFontSize());
 	setControlsFont(newFont);
-	//setControlsColor(QColor::fromRgbF(0.9, 0.91, 0.95, 0.9));
 
 	//Traditional field width from Ocular ;)
 	QFontMetrics fm(fieldOcularName->font());
@@ -381,7 +372,6 @@ OcularsGuiPanel::OcularsGuiPanel(Oculars* plugin,
 	connect(rotateCcdPlus5Button,   SIGNAL(triggered()), this, SLOT(updateCcdControls()));
 	connect(rotateCcdPlus15Button,  SIGNAL(triggered()), this, SLOT(updateCcdControls()));
 	connect(resetCcdRotationButton, SIGNAL(triggered()), this, SLOT(updateCcdControls()));
-
 
 	//Set the layout and update the size
 	qreal width = 2*prevOcularButton->boundingRect().width() + maxWidth;
@@ -1071,9 +1061,9 @@ void OcularsGuiPanel::updateMainButtonsPositions()
 		width += 2 * prevTelescopeButton->getButtonPixmapWidth();
 		posX = prevTelescopeButton->getButtonPixmapWidth();
 	}
-	if (buttonOcular->parentItem())
+	if ( (buttonOcular->parentItem()) && (buttonOcular->parentItem()->parentItem()) )
 	{
-		qreal parentWidth = buttonOcular->parentItem()->boundingRect().width();
+		qreal parentWidth = buttonOcular->parentItem()->parentItem()->boundingRect().width();
 		int nGaps = n - 1;//n buttons have n-1 gaps
 		spacing = qRound((parentWidth-width)/nGaps);
 	}

plugins/Oculars/src/gui/ocularDialog.ui
@@ -196,7 +196,41 @@
              <property name="bottomMargin">
               <number>0</number>
              </property>
-             <item row="8" column="0">
+             <item row="3" column="0">
+              <layout class="QHBoxLayout" name="guiPanelHorizontalLayout">
+               <item>
+                <widget class="QCheckBox" name="checkBoxControlPanel">
+                 <property name="text">
+                  <string>On-screen control panel</string>
+                 </property>
+                </widget>
+               </item>
+               <item>
+                <widget class="QLabel" name="guiFontSizeLabel">
+                 <property name="text">
+                  <string>Font size</string>
+                 </property>
+                 <property name="alignment">
+                  <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
+                 </property>
+                </widget>
+               </item>
+               <item>
+                <widget class="QSpinBox" name="guiFontSizeSpinBox">
+                 <property name="minimum">
+                  <number>10</number>
+                 </property>
+                 <property name="maximum">
+                  <number>30</number>
+                 </property>
+                 <property name="value">
+                  <number>12</number>
+                 </property>
+                </widget>
+               </item>
+              </layout>
+             </item>
+             <item row="11" column="0">
               <layout class="QHBoxLayout" name="horizontalLayout_8">
                <item>
                 <widget class="QLabel" name="arrowButtonScaleLabel">
@@ -226,21 +260,14 @@
                </item>
               </layout>
              </item>
-             <item row="5" column="0">
+             <item row="8" column="0">
               <widget class="QCheckBox" name="checkBoxResolutionCriterion">
                <property name="text">
                 <string>Show resolution criteria</string>
                </property>
               </widget>
              </item>
-             <item row="2" column="0">
-              <widget class="QCheckBox" name="checkBoxControlPanel">
-               <property name="text">
-                <string>On-screen control panel</string>
-               </property>
-              </widget>
-             </item>
-             <item row="3" column="0">
+             <item row="6" column="0">
               <widget class="QCheckBox" name="checkBoxInitialFOV">
                <property name="toolTip">
                 <string>Mark it if you want the FOV value the same like at the startup of Stellarium.</string>
@@ -250,7 +277,7 @@
                </property>
               </widget>
              </item>
-             <item row="4" column="0">
+             <item row="7" column="0">
               <widget class="QCheckBox" name="checkBoxInitialDirection">
                <property name="toolTip">
                 <string>Mark it if you want the direction value the same like at the startup of Stellarium.</string>
@@ -260,7 +287,7 @@
                </property>
               </widget>
              </item>
-             <item row="6" column="0">
+             <item row="9" column="0">
               <widget class="QCheckBox" name="checkBoxToolbarButton">
                <property name="text">
                 <string>Show oculars button on toolbar</string>

xdbgjhjreedwef/stelscope

commit 14c328aae36fc52bd1d225d8692363d8066d8dbf

Fixed wrong calculation the FOV of CCD in Oculars plugin (do not use first lens when any lens is not selected!)
The bug was introduced in revision
7d31bbb640549cad06309d59ac711ff62ca314f9

Workaround for version 0.18.2: add lens with multiplier 1 at first place
into list of lenses.

plugins/Oculars/src/Oculars.cpp
@@ -681,7 +681,7 @@ void Oculars::init()
 		}
 		else
 		{
-			selectedTelescopeIndex = qMin(settings->value("telescope_index", 0).toInt(), actualTelescopeCount-1);;
+			selectedTelescopeIndex = qMin(settings->value("telescope_index", 0).toInt(), actualTelescopeCount-1);
 		}
 
 		int lensCount = settings->value("lens_count", 0).toInt();
@@ -702,7 +702,7 @@ void Oculars::init()
 		{
 			qWarning() << "The Oculars ini file appears to be corrupt; delete it.";
 		}
-		selectedLensIndex=qMin(settings->value("lens_index", 0).toInt(), actualLensCount-1);;
+		selectedLensIndex=qMin(settings->value("lens_index", -1).toInt(), actualLensCount-1); // Lens is not selected by default!
 
 		pxmapGlow = new QPixmap(":/graphicGui/glow32x32.png");
 		pxmapOnIcon = new QPixmap(":/ocular/bt_ocular_on.png");